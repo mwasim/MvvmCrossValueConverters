4780b6219f7c7e13c0e731c78a0d8e9de66e11bb
mwasim <mwasimse@gmail.com>
2019-12-30 19:36:52 +0500

    Created iOS proect, and added HomeView

---
+++ MvvmCrossValueConversion.iOS/AppDelegate.cs
@@ -0,0 +1,15 @@
+﻿using Foundation;
+using MvvmCross.Platforms.Ios.Core;
+using UIKit;
+
+namespace MvvmCrossValueConversion.iOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+    [Register("AppDelegate")]
+    public class AppDelegate : MvxApplicationDelegate<MvxIosSetup<Core.App>, Core.App>
+    {
+    }
+}
+
+
---
+++ MvvmCrossValueConversion.iOS/Views/HomeView.cs
@@ -0,0 +1,35 @@
+﻿using Foundation;
+using MvvmCross.Binding.BindingContext;
+using MvvmCross.Platforms.Ios.Binding.Views;
+using MvvmCross.Platforms.Ios.Views;
+using MvvmCrossValueConversion.Core.ViewModels;
+
+namespace MvvmCrossValueConversion.iOS.Views
+{
+    [Register("HomeView")]
+    public class HomeView : MvxTableViewController
+    {
+        public HomeView()
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            var source = new MvxStandardTableViewSource(TableView, "TitleText Title; SelectedCommand ShowCommand");
+
+            /*
+            var set = this.CreateBindingSet<HomeView, HomeViewModel>();
+            set.Bind(source).To(vm => vm.MenuItems);
+            set.Apply();
+            */
+            
+            //simple binding syntax
+            this.CreateBinding(source).To((HomeViewModel vm) => vm.MenuItems).Apply();
+
+            TableView.Source = source;
+            TableView.ReloadData();
+        }
+    }
+}
